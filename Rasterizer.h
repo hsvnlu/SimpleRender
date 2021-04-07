32c9029ccb494c15162e9b924575216f156c4872
hsvnlu <36412577+hsvnlu@users.noreply.github.com>
2021-04-07 12:16:22 +0800

    Update Rasterizer.h

--- Rasterizer.h
+++ Rasterizer.h
@@ -11,11 +11,11 @@ using namespace Eigen;
 
 class Rasterizer {
 public:
-	Rasterizer(unsigned int w, unsigned int h) { 
+	Rasterizer(unsigned int w, unsigned int h, Vector3f initColor) { 
 		width = w;
 		height = h;
 		depth_buffer = std::vector<float>(width * height, -std::numeric_limits<float>::infinity());
-		frame_buffer = std::vector<Vector3f>(width * height, Vector3f(0.0f, 0.0f, 0.0f));
+		frame_buffer = std::vector<Vector3f>(width * height, initColor);
 	}
 	void rendering(const Model model, const std::vector<Light>& lights, const Transformation& trans);
 	void rasterize_triangle(const Triangle& t, const std::array<Eigen::Vector3f, 3>& view_pos);
